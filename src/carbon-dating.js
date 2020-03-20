5c8b7d6d71911ba8553d310e5734877a899890c3
Ragneda <53915581+Ragneda@users.noreply.github.com>
2020-03-20 18:53:05 +0300

    refactor: add decision carbon

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,19 @@
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+    typeof sampleActivity === "string";
+    let num1 = parseFloat(sampleActivity);
+    let num = Math.log(MODERN_ACTIVITY / num1);
+    let num2 = 0.693 / HALF_LIFE_PERIOD;
+    let num3 = num / num2;
+    let number = Math.ceil(num3);
+
+
+    if (!isFinite(number) || number < 1) {
+        return false;
+    }
+
+    return number;
+
 };
