ce502dbd5789944e3c528e0ff557e65f6dd76ddd
Alena Rebkavets <archirebkavets@gmail.com>
2020-10-03 18:45:05 +0300

    fix: corrected wrong input parameter type

--- src/count-cats.js
+++ src/count-cats.js
@@ -2,7 +2,7 @@ module.exports = function countCats(backyard) {
   let arr = backyard.flat();
   let number = 0;
   for (let i = 0; i < arr.length; ++i) {
-    if (arr[i] === "^^") number++;
+    if (arr[i] === '^^') number++;
   }
   return number;
 };
