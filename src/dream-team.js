9b21470b09254f9af055198e17b075c38c719295
Alena Rebkavets <archirebkavets@gmail.com>
2020-10-04 23:55:10 +0300

    feat add solution what-season

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,24 +1,20 @@
 module.exports = function createDreamTeam(members) {
   let arr = [];
-  if(members === null || members === undefined) {
-    return false
+  if (members === null || members === undefined) {
+    return false;
   }
   for (i = 0; i < members.length; i++) {
     if (typeof members[i] === 'string') {
       arr.push(members[i].trim());
     }
   }
-  arr.sort()
   let str = '';
   for (i = 0; i < arr.length; i++) {
     if (typeof arr[i] === 'string') {
-    str += arr[i].slice(0, 1);
+      str += arr[i].slice(0, 1);
     }
   }
-
   let result = str.toUpperCase().split('').sort().join('');
-
   return result;
 };
-  // remove line with error and write your code here
-
+// remove line with error and write your code here
--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,20 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
+module.exports = function getSeason(date) {
+ if(date === undefined || date === '') {
+    return 'Unable to determine the time of year!'
+    }
+   
+let month = date.getMonth();
+if(month === 0 || month === 1 || month === 11) {
+  return 'winter'
+} else if(month === 2 || month === 3 || month === 4) {
+  return 'spring'
+}else if(month === 5 || month === 6 || month === 7) {
+  return 'summer'
+}else if(month === 8 || month === 9 || month === 10) {
+  return 'autumn'
+}else {
+  throw new Error('Error');
+}
+
   // remove line with error and write your code here
 };
